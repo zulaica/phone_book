c37052bebc3d3a564a261fa5a108b0e743525923
David Zulaica <zulaica@gmail.com>
2015-01-16 15:01:54 -0800

    Add ability to search for a contact by its id.

--- lib/contact.rb
+++ lib/contact.rb
@@ -28,4 +28,14 @@ class Contact
     @@contacts = []
   end
 
+  define_singleton_method(:find) do |identification|
+    found_contact = nil
+    @@contacts.each() do |contact|
+      if contact.id().eql?(identification.to_i())
+        found_contact = contact
+      end
+    end
+    found_contact
+  end
+
 end
