ea6b9c45b2c917d9dd76e38c852d7d1a77fcf6bc
David Zulaica <zulaica@gmail.com>
2015-01-16 14:55:29 -0800

    Organize specs better.

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -14,20 +14,20 @@ describe("Contact") do
     end
   end
 
+  describe("#store") do
+    it("also adds a unique ID automatically when a contact is stored.") do
+      the_contact = Contact.new({:given_name => "David", :middle_name => "Anthony", :surname => "Zulaica", :nickname => ""})
+      the_contact.store()
+      expect(the_contact.id()).to(eq(0))
+    end
+  end
+
   describe(".empty") do
     it("removes all contact objects from the contacts array.") do
       the_contact = Contact.new({:given_name => "David", :middle_name => "Anthony", :surname => "Zulaica", :nickname => ""})
       the_contact.store()
       Contact.empty()
       expect(Contact.all()).to(eq([]))
-    end
-  end
-
-  describe("#store") do
-    it("also adds a unique ID automatically when a contact is stored.") do
-      the_contact = Contact.new({:given_name => "David", :middle_name => "Anthony", :surname => "Zulaica", :nickname => ""})
-      the_contact.store()
-      expect(the_contact.id()).to(eq(0))
     end
   end
 
