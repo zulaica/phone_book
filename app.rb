4ddf15045d8f3cc5c6b383b906071d29ddc65bc1
David Zulaica <zulaica@gmail.com>
2015-01-16 11:55:36 -0800

    Add instance variables and methods necessary to add and retrive contacts.

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ also_reload("lib/**/*.rb")
 require("./lib/contact")
 
 get("/") do
+  @contacts = Contact.all()
   erb(:index)
+end
+
+post("/") do
+  @given_name = params.fetch("given_name")
+  @middle_name = params.fetch("middle_name")
+  @surname = params.fetch("surname")
+  @nickname = params.fetch("nickname")
+
+  the_contact = Contact.new({:given_name => @given_name, :middle_name => @middle_name, :surname => @surname, :nickname => @nickname})
+  the_contact.store()
+
+  redirect('/')
 end
