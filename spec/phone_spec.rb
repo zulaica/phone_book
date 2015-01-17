734f16898f607610704bc5175624bde3281cd939
David Zulaica <zulaica@gmail.com>
2015-01-16 16:24:32 -0800

    Revise spec to account for refactored Class methods.

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -7,7 +7,7 @@ describe("Phone") do
     it("creates a phone object and stores it into the associated Contact object.") do
       the_numbers = Phone.new({:home_phone => "(555) 555-5555", :work_phone => "(555) 555-5555", :mobile_phone => "(555) 555-5555", :other_phone => "(555) 555-5555"})
       the_numbers.store()
-      expect(Phone.all()).to(eq([the_numbers]))
+      expect(Phone.all()).to(eq(the_numbers))
     end
   end
 
