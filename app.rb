8fcc4d4895fa1c6d2376329103414d4a9f6ffbce
David Zulaica <zulaica@gmail.com>
2015-01-16 16:25:44 -0800

    Add logic to store and retrieve phone numbers to and from a contact.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/contact")
 require("./lib/phone")
+require("pry")
 
 get("/") do
   @contacts = Contact.all()
@@ -15,8 +16,14 @@ post("/") do
   @surname = params.fetch("surname")
   @nickname = params.fetch("nickname")
 
+  @home_phone = params.fetch("home_phone")
+  @work_phone = params.fetch("work_phone")
+  @mobile_phone = params.fetch("mobile_phone")
+  @other_phone = params.fetch("other_phone")
+
   the_contact = Contact.new({:given_name => @given_name, :middle_name => @middle_name, :surname => @surname, :nickname => @nickname})
-  the_contact.store()
+  the_numbers = Phone.new({:home_phone => @home_phone, :work_phone => @work_phone, :mobile_phone => @mobile_phone, :other_phone => @other_phone})
+  the_contact.store(the_numbers)
 
   redirect('/')
 end
